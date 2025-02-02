b75de0c16722aa1e4a094413ce6d19d1a4446451
mohamadAlrimi <117595968+mohamadAlrimi@users.noreply.github.com>
2025-02-02 16:24:17 +0300

    Building programing logic

--- src/LoanForm.js
+++ src/LoanForm.js
@@ -1,26 +1,50 @@
 
 import "./FormStyles.css" 
 import Modle from "./Modal"
+import { useState } from "react"
 export default function LoanForm(){
+    const[loanInputs,setLonaInputs   ]=useState({
+        name:"",
+        phoneNumber:"",
+        age:"",
+        isemployee :"false",
+        salaryRange:"",
+    })
+    function handlFormSubmit(event){
+        event.preventDefault();
+        alert("hello")
+
+    }
+    const btnIsDisabled = loanInputs.name === "" || loanInputs.age === "" || loanInputs.phoneNumber === "";
     return(<div className="flex" style={{flexDirection:"column"}}>
     <form  id="loan-form"className="flex" style={{flexDirection:"column"}}>
         <h1>Requesting a Loan </h1>
        <hr/>
         <label>Name:</label>
-        <input ></input>
+        <input value={loanInputs.name} onChange={(event)=>{
+            setLonaInputs({...loanInputs , name : event.target.value})
+        }} ></input>
         <label>Phone Number:</label>
-        <input ></input>
+        <input value={loanInputs.phoneNumber} onChange={(event)=>{
+            setLonaInputs({...loanInputs , phoneNumber : event.target.value})
+        }} ></input>
         <label>Age:</label>
-        <input ></input>
+        <input value={loanInputs.age} onChange={(event)=>{
+            setLonaInputs({...loanInputs , age : event.target.value})
+        }} ></input>
         <label style={{marginTop:"30px"}}>Are you an employee?</label> <br></br>
-        <input type="checkbox"/> <br/>
+        <input type="checkbox" value={loanInputs.isemployee} onChange={(event)=>{
+            setLonaInputs({...loanInputs , isemployee : event.target.checked})
+        }}  /> <br/>
         <label>Salary:</label>
-       <select>
+       <select value={loanInputs.salaryRange} onChange={(event)=>{
+            setLonaInputs({...loanInputs , salaryRange : event.target.value})
+        }} >
         <option>less than 500$</option>
         <option>between 500$ and 2000$</option>
         <option>above 2000$</option>
        </select>
-        <button id="submit-loan-btn">Submit</button>
+        <button onClick={handlFormSubmit} className={btnIsDisabled ? "disabled" : ""} disabled={btnIsDisabled} id="submit-loan-btn">Submit</button>
  
         </form>
         {/* <Modle/> */}
