600751fcc38f7b8932fd111b4bb3bd07b6deea50
mohamadAlrimi <117595968+mohamadAlrimi@users.noreply.github.com>
2025-03-03 23:51:02 +0300

    some change commit

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,7 +1,7 @@
 import MyInput from "./MyInput"
 export default function MyComponent( {valueComponent , handleChangeComponent ,inputNameComponent }){
     return(<>
-    <h1>This is the header of the component</h1>
+    {/* <h1>This is the header of the component</h1> */}
     <MyInput/>
     {/* <h1>This is the footer of the component</h1> */}
     
