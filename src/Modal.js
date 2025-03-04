8760e68432bb2c1308a41b2052dfca901ec85af1
mohamadAlrimi <117595968+mohamadAlrimi@users.noreply.github.com>
2025-03-04 22:41:18 +0300

    some changes

--- src/Modal.js
+++ src/Modal.js
@@ -4,6 +4,7 @@ export default function Modle({isVisible , errorMessage=null}){
     return(<div id="modal"><div id="modal-content">
         {/* <h1>The Form Has Been Submitted Successfully</h1> */}
         {/* <h1>The Form Has Been Submitted Successfully</h1> */}
+        {/* <h1>The Form Has Been Submitted Successfully</h1> */}
         <h1 style={{color : errorMessage ? "red" : "green"}}>{errorMessage != null ? errorMessage : "The Form Has Been Submitted Successfully"}</h1>
     
         </div></div>);
