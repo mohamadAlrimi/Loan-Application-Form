77b779e69395309cd860e46badbe58ee4f64f02c
mohamadAlrimi <117595968+mohamadAlrimi@users.noreply.github.com>
2025-03-06 21:06:02 +0300

    done1

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
     <UserContext.Provider value={{username :"muhammed" , name:"Muhammed Elrimi" , email:"muhammed12334.gmail.com"}}> <div className="App" style={{marginTop:"250px"}}>
     <LoanForm/>
     {/* this is commit  */}
+    {/* this is commit  */}
+    {/* this is commit  */}
       
     </div>   </UserContext.Provider>
    
